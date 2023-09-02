14e599cae983509bcb8875c65a09bbb35a1c1042
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-09-02 14:01:34 +0300

    create index.html route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var path = require('path');
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', '/public/html/index.html')); 
 });
 
 module.exports = router;
